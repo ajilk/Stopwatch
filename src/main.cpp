7abf59eb0671f5e1be498e5e3b8c52f973ae56c3
AzimeX <ajilkhomov@gmail.com>
2017-12-19 15:45:42 -0500

    foundation created

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,46 @@
 #include <ncurses.h>
+#include <chrono>
+bool kbhit() {
+	nodelay(stdscr, TRUE);
+	int ch = getch();
+    	if (ch != ERR) {
+       		//ungetch(ch);
+    		return true;
+  	 } else {
+     		return false;
+    	}
+	nodelay(stdscr, FALSE);
+}
 
 int main()
 {	
-	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
-
+	initscr();			/* Start curses mode		*/
+	start_color();
+	curs_set(0);
+	noecho();
+	cbreak();
+	int i=0;
+	while(getch() == ERR){
+		init_color(COLOR_GREEN, i*3, i*3, i*3);
+		init_pair(1, COLOR_BLACK, COLOR_GREEN);
+		attron(COLOR_PAIR(1));
+		mvprintw(0,0," "); refresh(); i++;
+		attroff(COLOR_PAIR(2));
+		nodelay(stdscr, TRUE);
+	}
+	auto start = std::chrono::steady_clock::now();
+	auto end = std::chrono::steady_clock::now();
+	nodelay(stdscr, TRUE);
+	do{
+		end = std::chrono::steady_clock::now();
+		mvprintw(0,0,"%.3f", std::chrono::duration_cast<std::chrono::duration<float> >(end-start).count());
+		refresh();
+	}while(getch() == ERR);
+	nodelay(stdscr, FALSE);
+	end = std::chrono::steady_clock::now();
+	mvprintw(0,0,"%.3f", std::chrono::duration_cast<std::chrono::duration<float> >(end-start).count());
+	refresh();
+	getch();
+	endwin();			/* End curses mode 		*/
 	return 0;
 }
