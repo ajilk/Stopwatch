fb36f3749fda254f350c65fd8687191b5f831c47
AzimeX <ajilkhomov@gmail.com>
2017-12-19 12:26:15 -0500

    updated with ncurses

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <ncurses.h>
 
-int main(){
+int main()
+{	
+	initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
+
 	return 0;
 }
